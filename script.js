b0d20b34e963e6e1d30201b3948e96bf0ef860dc
peppogp <145770790+peppogp@users.noreply.github.com>
2024-02-05 20:14:48 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function removeTwitterLink() {
+    const twitterLink = document.querySelector(".blog-sidebar .p-4:nth-of-type(3) .list-unstyled li:nth-of-type(2)");
+    twitterLink.remove()
+}
+
+removeTwitterLink()
